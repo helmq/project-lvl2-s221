69b259b57dcab6e2cc2658cf217d678379335566
Viktor <jarlaks52@gmail.com>
2018-02-19 17:08:41 +0900

    update help

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import commander from 'commander';
 
 export default () => {
   const program = commander
+    .version('0.0.4')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
