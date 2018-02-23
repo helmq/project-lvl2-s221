2640904270816e1422dec86b77ad82700aa83b9d
Viktor <jarlaks52@gmail.com>
2018-02-24 00:47:04 +0900

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,36 +1,41 @@
 import gendiff from '../src';
-import { flat as flatJSON, recursive as recursiveJSON } from './__fixtures__/json/expected';
+import { flat as flatJSON, recursive as recursiveJSON, plain } from './__fixtures__/json/expected';
 import { flat as flatYAML, recursive as recursiveYAML } from './__fixtures__/yaml/expected';
 import { flat as flatIni, recursive as recursiveIni } from './__fixtures__/ini/expected';
 
 const fixturesDir = './__tests__/__fixtures__';
 
-test('flat JSON', () => {
-  const result = gendiff(`${fixturesDir}/json/first.json`, `${fixturesDir}/json/second.json`);
+test('flat JSON recurse', () => {
+  const result = gendiff(`${fixturesDir}/json/first.json`, `${fixturesDir}/json/second.json`, 'recurse');
   expect(result).toBe(flatJSON);
 });
 
-test('recursive JSON', () => {
-  const result = gendiff(`${fixturesDir}/json/r.first.json`, `${fixturesDir}/json/r.second.json`);
+test('recursive JSON recurse', () => {
+  const result = gendiff(`${fixturesDir}/json/r.first.json`, `${fixturesDir}/json/r.second.json`, 'recurse');
   expect(result).toBe(recursiveJSON);
 });
 
-test('flat YAML', () => {
-  const result = gendiff(`${fixturesDir}/yaml/first.yaml`, `${fixturesDir}/yaml/second.yaml`);
+test('recursive JSON plain', () => {
+  const result = gendiff(`${fixturesDir}/json/r.first.json`, `${fixturesDir}/json/r.second.json`, 'plain');
+  expect(result).toBe(plain);
+});
+
+test('flat YAML recurse', () => {
+  const result = gendiff(`${fixturesDir}/yaml/first.yaml`, `${fixturesDir}/yaml/second.yaml`, 'recurse');
   expect(result).toBe(flatYAML);
 });
 
-test('recursive YAML', () => {
-  const result = gendiff(`${fixturesDir}/yaml/r.first.yaml`, `${fixturesDir}/yaml/r.second.yaml`);
+test('recursive YAML recurse', () => {
+  const result = gendiff(`${fixturesDir}/yaml/r.first.yaml`, `${fixturesDir}/yaml/r.second.yaml`, 'recurse');
   expect(result).toBe(recursiveYAML);
 });
 
-test('flat ini', () => {
-  const result = gendiff(`${fixturesDir}/ini/first.ini`, `${fixturesDir}/ini/second.ini`);
+test('flat ini recurse', () => {
+  const result = gendiff(`${fixturesDir}/ini/first.ini`, `${fixturesDir}/ini/second.ini`, 'recurse');
   expect(result).toBe(flatIni);
 });
 
-test('recursive ini', () => {
-  const result = gendiff(`${fixturesDir}/ini/r.first.ini`, `${fixturesDir}/ini/r.second.ini`);
+test('recursive ini recurse', () => {
+  const result = gendiff(`${fixturesDir}/ini/r.first.ini`, `${fixturesDir}/ini/r.second.ini`, 'recurse');
   expect(result).toBe(recursiveIni);
 });
